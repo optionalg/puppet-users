29366a2ae0a3d954bbcf5e5372a3dbe51750ffa6
Brandon Helwig <brhelwig@brhelwig.com>
2012-05-23 17:08:18 -0600

    first attempt at user management module

---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+define users::create(
+    $name = '',
+    $group = 'users',
+    $ensure = 'present'
+    $comment = '',
+    $home = "/home/${name}",
+    $managehome = true,
+    $password = '',
+    $shell = '/bin/sh',
+    $system = false,
+    $ssh_key = ''
+    $ssh_key_type = 'ssh-rsa'
+) {
+
+user { $name : 
+    comment     => $comment,
+    ensure      => $ensure,
+    gid         => $group,
+    home        => "/home/${name}",
+    managehome  => $managehome,
+    password    => sha1($password),
+    shell       => $shell,
+    system      => $system
+    }
+
+ssh_authorized_key { "$name-primary" : 
+    ensure  => $ensure,
+    key     => $ssh_key,
+    type    => $ssh_key_type,
+    user    => $name
+    }
+
+}
